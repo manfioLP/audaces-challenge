0af9566350978bb0506bb94da18a116817414fdd
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-09-27 16:27:45 -0300

    add type MovieList

--- src/model/Movie.ts
+++ src/model/Movie.ts
@@ -3,4 +3,21 @@ export class Movie {
     episode: number
     director: string
     opening_crawl: string
-  }
+}
+
+export class MovieList {
+  characters: string[]
+  created: string
+  director: string
+  edited: string
+  episode_id: number
+  opening_crawl: string
+  planets: string[]
+  producer: string
+  release_date: string
+  species: string[]
+  starships: string[]
+  title: string
+  url: string
+  vehicles: string[]
+}
